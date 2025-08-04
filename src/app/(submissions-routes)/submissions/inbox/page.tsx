c007fb858697dad8b5db74dd5a4de8178ec9d9df
Dioni M <me@dionimercado.com>
2025-08-04 13:58:05 -0400

    Submissions inbox page - E2E (mocked)

--- src/app/(submissions-routes)/submissions/inbox/page.tsx
+++ src/app/(submissions-routes)/submissions/inbox/page.tsx
@@ -23,6 +23,13 @@ export default function SubmissionsWorklistPage() {
         header: () => <span>From</span>,
       },
       {
+        accessorFn: (row) => row.IsRush,
+        id: 'IsRush',
+        cell: (info) => info.getValue(),
+        header: 'Important',
+        footer: (props) => props.column.id,
+      },
+      {
         accessorFn: (row) => row.EmailDetails?.subject,
         id: 'EmailDetails.subject',
         cell: (info) => info.getValue(),
@@ -45,52 +52,6 @@ export default function SubmissionsWorklistPage() {
         footer: (props) => props.column.id,
       },
       {
-        accessorFn: (row) => row.SubmissionStatusDescription,
-        id: 'SubmissionStatusDescription',
-        cell: (info) => info.getValue(),
-        header: 'Status',
-        footer: (props) => props.column.id,
-      },
-      {
-        accessorFn: (row) => row.AssignedToName,
-        id: 'AssignedToName',
-        cell: (info) => info.getValue(),
-        header: 'Assigned To',
-        footer: (props) => props.column.id,
-      },
-      {
-        accessorFn: (row) => row.SubmissionId,
-        id: 'SubmissionId',
-        cell: (info) => info.getValue(),
-        header: 'ID',
-        footer: (props) => props.column.id,
-      },
-      {
-        accessorKey: 'comments',
-        cell: (info) => (
-          <button className="cursor-pointer">
-            <Image
-              src={info.getValue() ? commentFilledIcon : commentIcon}
-              alt="Edit"
-              width={21}
-              height={19}
-            />
-          </button>
-        ),
-        header: 'Comments',
-        footer: (props) => props.column.id,
-      },
-      {
-        accessorKey: 'edit',
-        cell: () => (
-          <button className="cursor-pointer">
-            <EditPenIcon />
-          </button>
-        ),
-        header: 'Edit',
-        footer: (props) => props.column.id,
-      },
-      {
         accessorKey: 'actions',
         cell: () => (
           <button className="hover:bg-primary-light inline-block cursor-pointer rounded-sm px-3 py-1">
@@ -104,12 +65,12 @@ export default function SubmissionsWorklistPage() {
     []
   )
 
-  const { items: submissions } = useSubmissions('worklist') // Fetch submissions for worklist
+  const { items: submissions } = useSubmissions('inbox') // Fetch submissions for inbox
 
   return (
     <div className="p-6">
       <div className="">
-        <PageHeader title="Worklist" />
+        <PageHeader title="Inbox" />
         <section className="-mt-px">
           <CustomTable
             {...{
--- src/app/(submissions-routes)/submissions/page.tsx
+++ src/app/(submissions-routes)/submissions/page.tsx
@@ -1,4 +1,11 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
 export default function SubmissionsPage() {
+  const router = useRouter()
+
+  router.push('/submissions/inbox')
   return (
     <>
       <h1>Submissions</h1>
