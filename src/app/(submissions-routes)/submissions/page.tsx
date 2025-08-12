7ee56778f5cbac51e06863ea4f8f282f4f15a1a5
Dioni M <me@dionimercado.com>
2025-08-12 10:52:34 -0400

    Fix submissions redirect warning

--- src/app/(submissions-routes)/submissions/page.tsx
+++ src/app/(submissions-routes)/submissions/page.tsx
@@ -1,15 +1,5 @@
-'use client'
-
-import { LoaderSpinner } from '@/features/ui/LoaderSpinner'
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 export default function SubmissionsPage() {
-  const router = useRouter()
-
-  router.push('/submissions/inbox')
-  return (
-    <div className="flex items-center justify-center h-64">
-      <LoaderSpinner />
-    </div>
-  )
+  redirect('/submissions/inbox')
 }
